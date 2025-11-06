d4f05e015068367e71100f7623411aab9aa96c85
Junior Soares <jjwsoares@outlook.com>
2025-11-06 16:40:35 -0300

    Correcao no middleware de upload
    Agora quando a pasta uploads nao existir ira ser criada automaticamente

--- src/middlewares/uploadMiddleware.ts
+++ src/middlewares/uploadMiddleware.ts
@@ -1,24 +1,25 @@
-import multer from "multer";
-import path from "node:path";
+import multer from "multer"
+import path from "node:path"
+import fs from "node:fs"
 import { fileURLToPath } from "node:url"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// Configura o armazenamento dos arquivos utilizando o diskStorage do multer.
+const uploadDir = path.resolve(__dirname, "../../public/uploads")
+
+fs.mkdirSync(uploadDir, { recursive: true })
+
 const storage = multer.diskStorage({
-    // Define o diretório de destino onde os arquivos serão armazenados.
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../../public/uploads"))
+        fs.mkdirSync(uploadDir, { recursive: true })
+        cb(null, uploadDir)
     },
-    // Define o nome do arquivo armazenado.
     filename: (req, file, cb) => {
-        // O nome do arquivo será composto pelo timestamp atual e a extensão original do arquivo.
         const ext = path.extname(file.originalname);
-        const unique = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        const unique = `${Date.now()}-${Math.round(Math.random() * 1e9)}`
         cb(null, `${unique}${ext}`)
     }
-})
+});
 
-// Exporta o middleware de upload configurado com o armazenamento definido.
-export const upload = multer({ storage: storage })
+export const upload = multer({ storage })
