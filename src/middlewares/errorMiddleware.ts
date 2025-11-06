9519d3493d662711f9eb0a3ae38c6b5701260928
Junior Soares <jjwsoares@outlook.com>
2025-11-06 16:39:17 -0300

    Pequena corracao no middleware de error

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -3,10 +3,10 @@ import { HttpError } from "../errors/HttpError.js";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if(err instanceof HttpError){
-        const alert = {message: err.message, status: err.status}
+        let messages = {success: [], error: [{title: "Erro", message: err.message}]}   
 
         if(!res.locals.user){
-            res.render("login", {alert})
+            res.render("login", {messages})
         }
     } else if (err instanceof Error) {
         res.status(500).json({ error: "Erro interno do servidor", details: err.message })
